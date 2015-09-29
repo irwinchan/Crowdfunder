449f9c9fbd9c2f4a170dc22daed2d380342ba191
Heart Beat Studio <blacklight728@gmail.com>
2015-09-28 21:09:34 -0400

    Add Projects controller actions & views

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,4 +2,45 @@ class ProjectsController < ApplicationController
   def index
     @projects = Project.order(ended_at: :desc)
   end
+
+  def new
+    @project = Project.new
+  end
+
+  def show
+    @project = Project.find(params[:id])
+  end
+
+  def create
+    @project = Project.new(projects_params)
+    if @project.save
+      redirect_to projects_path, notice: "Project created successfully!"
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @project = Project.find(params[:id])
+  end
+
+  def update
+    @project = Project.find(params[:id])
+    if @project.update_attributes(projects_params)
+      redirect_to projects_path, notice: "Project Info Updated!"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @project = Project.find(params[:id])
+    @project.destroy
+    redirect_to projects_path, notice: "Project deleted!"
+  end
+
+  private 
+  def projects_params
+    params.require(:project).permit(:name, :description, :funding_goal, :started_at, :ended_at)
+  end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,7 @@
 class Project < ActiveRecord::Base
+  # belongs_to :user
   has_many :rewards
+  # has_many :pledges
+  # has_many :users, through :pledges
+  # has_many :pledges, through :rewards
 end
