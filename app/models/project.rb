e624d8aaa47678400220b6f9a8faeee01b636ae0
Heart Beat Studio <blacklight728@gmail.com>
2015-09-29 15:46:07 -0400

    Add cocoon form for rewards

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,7 @@
 class Project < ActiveRecord::Base
   # belongs_to :user
   has_many :rewards
+  accepts_nested_attributes_for :rewards, reject_if: :all_blank, allow_destroy: true
   # has_many :pledges
   # has_many :users, through :pledges
   # has_many :pledges, through :rewards
