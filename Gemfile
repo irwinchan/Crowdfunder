fd337518bcf70940150a4aa34e7eef2e7bb5ac86
Irwin <irwinchan@me.com>
2015-09-30 17:50:35 -0400

    Create seed data for rewards using faker

--- Gemfile
+++ Gemfile
@@ -47,5 +47,8 @@ group :development do
 
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
+
+  # faker for fake seed data
+  gem 'faker'
 end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# 10.times do
+#  Project.create!(
+#    name: Faker::Commerce.product_name,
+#    description: Faker::Lorem.sentence,
+#    started_at: Faker::Date.forward(23),
+#    ended_at: Faker::Date.forward(50),
+#    funding_goal: rand(50000..200000)
+#  )
+# end
+
+30. times do
+  Reward.create!(
+     name: Faker::Commerce.product_name,
+     description: Faker::Lorem.sentence,
+     backer_limit: rand(10..200),
+     project_id: rand(1..10)
+   )
+end
