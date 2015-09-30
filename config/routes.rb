2828fdad56c3c3db4bf1b43bee19d7ff8df39223
Irwin <irwinchan@me.com>
2015-09-30 18:51:02 -0400

    Add routes for pledges

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'projects#index'
   resources :users, only: [:show, :new, :create]
+  resources :user_sessions
   resources :projects do
     resources :rewards, only: [:show, :new, :create] do
       resources :pledges, only: [:show, :new, :create]
